0ce6029378ee84476e2ee6a66d8e2f133f6a1834
aurelioo29 <aureliolucio5@gmail.com>
2025-12-14 13:20:27 +0700

    feat: improve middleware for update last seen

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\CanManageUsers;
+use App\Http\Middleware\UpdateLastSeen;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->appendToGroup('web', UpdateLastSeen::class);
         $middleware->alias([
             'can_manage_users' => CanManageUsers::class,
         ]);
